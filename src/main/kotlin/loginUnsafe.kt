950139c11616a01c372b5c574c0ba2178e33dfd5
FivesoftCode <80209022+FivesoftCode@users.noreply.github.com>
2025-01-22 12:12:04 +0100

    Update loginUnsafe.kt

--- src/main/kotlin/loginUnsafe.kt
+++ src/main/kotlin/loginUnsafe.kt
@@ -10,17 +10,6 @@ fun loginUnsafe(username: String, password: String): Boolean {
     return resultSet.next()
 }
 
-fun loginSafe(username: String, password: String): Boolean {
-    val conn: Connection = DriverManager.getConnection("jdbc:sqlite:users.db")
-    val query = "SELECT * FROM users WHERE username = ? AND password = ?"
-    val preparedStatement = conn.prepareStatement(query)
-    preparedStatement.setString(1, username)
-    preparedStatement.setString(2, password)
-
-    val resultSet = preparedStatement.executeQuery()
-    return resultSet.next()
-}
-
 fun main() {
     println("Podaj login: ")
     val username = readln()
